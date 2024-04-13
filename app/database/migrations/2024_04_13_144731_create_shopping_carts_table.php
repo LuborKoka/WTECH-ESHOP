8db5479a64775bebbc7eb39d10d7505ed5815b82
LuborKoka <koka.lubor87@gmail.com>
2024-04-13 18:34:47 +0200

    Update 2024_04_13_144731_create_shopping_carts_table.php

--- app/database/migrations/2024_04_13_144731_create_shopping_carts_table.php
+++ app/database/migrations/2024_04_13_144731_create_shopping_carts_table.php
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('shopping_carts_');
+        Schema::dropIfExists('shopping_carts');
     }
 };
